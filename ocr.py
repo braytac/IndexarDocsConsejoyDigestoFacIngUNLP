ae186537e939bf73f189960b62e727a187647ab4
Henry <zat_xorg@hotmail.com>
2018-11-17 16:10:33 -0300

    Corrección de errores y mejoras
    - Añadido localmente pdfminer para PDF->TXT->SQL en para los no-escaneados (editables)
    - Añadidas nuevas rutas
    - Except locales para evitar que un documento problemático cause una excepción en pdfminer y salga del programa
    - Uso de gs para conversión a tiff en lugar de conver (alto % cpu y excesivos temporales)
    - Silenciar tesseract
    - Mostar archivos que están en DB y ya no existen en FS

--- ocr.py
+++ ocr.py
@@ -1,27 +1,10 @@
 #!/usr/bin/env python2
 """
-Docstring para que pylint no joda
-recordar en mysql setear max_allowed_packet = 100M para poder hacer insert
-sys.getdefaultencoding() devuelve default encoding para python ASCII,
-pero el FS en linux es utf8 -> se descajetan los nombres de archivo
-agregar esto, o manejar senales:
-try:
-    while True:
-      print 1
-except KeyboardInterrupt:
-    print "test"
-,tener en cuenta terminar tesseract y convert si estan a mitad de camino
-Si en alguna distro tira algo como:
-
-convert: attempt to perform an operation not allowed by the security policy `PDF' @ error/module.c/OpenModule/1257.
-convert: no decode delegate for this image format `PDF' @ error/constitute.c/ReadImage/556.
-convert: no images defined `file.tiff' @ error/convert.c/ConvertImageCommand/3288.
-
-, modificar policy.xml:
-
+En el caso de convert modificar policy.xml:
 <policy domain="module" rights="read|write" pattern="{EPS,PS2,PS3,PS,PDF,XPS}" />
-
 """
+import sys
+sys.path.append('pdfminer')
 import os
 import io
 import fnmatch
@@ -31,6 +14,13 @@ from mysql.connector import errorcode
 from subprocess import PIPE,Popen
 import datetime
 
+from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
+from pdfminer.converter import TextConverter
+from pdfminer.layout import LAParams
+from pdfminer.pdfpage import PDFPage
+from io import BytesIO
+
+
 #import sys
 #reload(sys)
 #sys.setdefaultencoding("UTF-8")
@@ -39,6 +29,25 @@ import datetime
 #print sys.getdefaultencoding()
 
 procesarPDF = 1
+
+def pdf_to_text(path):
+    manager = PDFResourceManager()
+    retstr = BytesIO()
+    layout = LAParams(all_texts=True)
+    device = TextConverter(manager, retstr, laparams=layout)
+    filepath = open(path, 'rb')
+    interpreter = PDFPageInterpreter(manager, device)
+
+    for page in PDFPage.get_pages(filepath, check_extractable=True):
+        interpreter.process_page(page)
+
+    text = retstr.getvalue()
+
+    filepath.close()
+    device.close()
+    retstr.close()
+    return text,len(text)
+
 
 def modification_date(filename):
 
@@ -75,12 +84,12 @@ def getData(sql):
     cnx.close()
     return cursor.fetchall()
 
-def insertar( sql, nombre_archivo, contenido ):
+def insertar( sql, tipo, nombre_archivo, contenido ):
 
     cnx = conectarMySQL()
     cursor = cnx.cursor()
     if len(nombre_archivo) != 0:
-        cursor.execute(sql, (nombre_archivo, contenido))
+        cursor.execute(sql, (tipo, nombre_archivo, contenido))
         cnx.commit() # aplicar
         cnx.close()
     else:
@@ -113,128 +122,369 @@ def find_files(directory, patron):
 
 
 def lista_diferencia_DBFS(ruta,patron,sql):
-
+    lista1 = []
     lista_archivosDB = []
     countinDB = 0
     for t in getData(sql):  # ->cursor.fetchall
         lista_archivosDB.append(t[0])
-        #print t[0]
+        #if t[0] == '/var/www/intranet/repo/documentos/sesion-8-2018/Acta 7 sesion (oct.).doc':
+            #lista1.append(t[0].decode('windows-1252'))
+            #print "DB tiene: "+t[0].decode('windows-1252')
         countinDB = countinDB + 1
+
+    lista2 = []
     archivos_enFS = []
     #print "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
 
     countinFS = 0
-
     for nombre_archivo in find_files(ruta, patron):
+        #if nombre_archivo == '/var/www/intranet/repo/documentos/sesion-8-2018/Acta 7 sesion (oct.).doc':
+            #lista2.append(nombre_archivo)
+            #print "FS TIENE: "+nombre_archivo
         archivos_enFS.append(nombre_archivo)
         countinFS = countinFS + 1
 
+    #diff_temp = []
+    #diff_temp2 = []
+    #diff_temp = list(set(lista1) - set(lista2))
+    #diff_temp2 = list(set(lista2) - set(lista1))
+    """
+    print "1-2:"+ruta
+    for n in diff_temp:
+        print "[ DB -FS ] "+ruta+" ->"+n
+
+    print "2-1:"+ruta
+    for n in diff_temp2:
+        print "[ FS - DB ] "+ruta+" ->"+n
+    """  
+    diffrev = []
+    diffrev = list(set(lista_archivosDB) - set(archivos_enFS))
+   
+    if diffrev:
+       print "Archivos en DB que ya no estan en FS: "
+    for n in diffrev:
+        print "     [DB-FS]: "+n   
+
     diff = []
     diff = list(set(archivos_enFS) - set(lista_archivosDB))
+
     return diff, countinDB, countinFS
 
 
-def procesar_pdf(diff):
+def procesar_pdf(diff, procesarPDF, tipo):
 
     for nombre_archivo in diff:
-        print "-> "+nombre_archivo
+        print "-> Indexando "+nombre_archivo
 
         if procesarPDF == 1:
             os.system('cp "'+nombre_archivo+'" tmp.pdf')
             #os.system('convert -density 300 tmp.pdf -depth 8 -strip -background white -alpha off file.tiff')
-            os.system('gs -dNOPAUSE -dINTERPOLATE  -r150 -dGraphicsAlphaBits=4 -dNumRenderingThreads=8 -sDEVICE=tiff32nc  -o file.tiff tmp.pdf')
+            #gs -dBATCH -dNOPAUSE -sDEVICE=txtwrite -sOutputFile=1.txt res-01404-2018.pdf
             # tiffg4 muy matricial, aun con r600 sale cualquier banana. Usando tiff no B/N, con resolucion no muy alta para
             # evitar archivo inmenso
             #os.system('gs -dNOPAUSE -r600 -dINTERPOLATE  -dNumRenderingThreads=8 -sDEVICE=tiffg4 -o file.tiff tmp.pdf')
             #os.system('convert -density 300 "'+nombre_archivo+'" -depth 8 -strip -background white -alpha off file.tiff')
-            os.system("tesseract file.tiff ocr -l spa")
-            ocr_txt = io.open("ocr.txt", "r", encoding="utf-8")
-            contenido = ocr_txt.read()
-            contenido = " ".join(contenido.split())
-            sql =  "INSERT IGNORE INTO ocr (tipo,archivo,contenido) VALUES ( '3' , %s , %s )"
-            insertar(sql, nombre_archivo, contenido)
+            try:
+                txt,longitud = pdf_to_text("tmp.pdf")
+                #print "LONGITUD:  "+str(longitud)
+            except AttributeError:
+                print "Excepcion AttributeError de  pdfminer"
+                pass
+            except:
+                print "Excepcion en pdfminer"
+                pass
+            try: 
+                 if longitud < 100:
+                     print "Conversion & OCR..."
+                     os.system('gs -dNOPAUSE -dINTERPOLATE  -r150 -dGraphicsAlphaBits=4 -dNumRenderingThreads=8 -sDEVICE=tiff32nc  -o file.tiff tmp.pdf > /dev/null 2>&1')
+                     os.system("tesseract file.tiff ocr -l spa > /dev/null 2>&1 ")
+                     ocr_txt = io.open("ocr.txt", "r", encoding="utf-8")
+                     contenido = ocr_txt.read()
+                     contenido = " ".join(contenido.split())
+                 else:
+                     contenido = txt
+                     #print "PDF CON TEXTO..." 
+            except: 
+                print "Excepcion en gs o tesseract"
+                pass
+            sql =  "INSERT IGNORE INTO ocr (tipo,archivo,contenido) VALUES ( %s , %s , %s )"
+            insertar(sql, tipo, nombre_archivo, contenido)
             os.system("rm -f file.tiff && rm -f ocr.txt && rm -f tmp.pdf && find /tmp -maxdepth 1 -type f -name 'magick-*' -delete")
             #raw_input("Press Enter to continue...")
 
-def procesar_words(diff):
+def procesar_words(diff, tipo):
 
     for nombre_archivo in diff:
         # cuidado al imprimir, con nohup tira error por bad encoding. Derecho sin nohup no tira error :O
         print "-> "+nombre_archivo
         #arch = subprocess.check_output("php extraertxt_de_docs.php?"+nombre_archivo, shell=True);
         # subprocess.check_output(["... no funca, porque es python < 2.7 :(
-
+        #Popen(['gs', '-dBATCH','-dNOPAUSE','-sDEVICE=txtwrite','-sOutputFile=a.txt','/tmp/res-01404-2018.pdf'], stdout=PIPE,stderr=None)
         contenidoDOC = Popen(['php', 'extraertxt_de_docs.php', nombre_archivo], stdout=PIPE)
-        sql =  '''INSERT IGNORE INTO ocr (tipo,archivo,contenido) VALUES ( '3' , %s , %s )'''
-        insertar(sql, nombre_archivo, contenidoDOC.communicate()[0])
+        sql =  '''INSERT IGNORE INTO ocr (tipo,archivo,contenido) VALUES ( %s , %s , %s )'''
+        insertar(sql, tipo, nombre_archivo, contenidoDOC.communicate()[0])
 
 
 try: 
     # Y LUEGO REVEER los que se guardaron vacios-> WHERE LENGTH(contenido)='0'
 
-    ###################### DIGESTO (.DOC*)  #######################
+    ###################### RESOLUCIONES  FI (.DOC*)  #######################
 
-    sql = " SELECT archivo FROM ocr WHERE tipo='3'AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+    sql = " SELECT archivo FROM ocr WHERE tipo='1' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
 
-    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/intranet/repo/digesto','*.doc*',sql)
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/facultad/resoluciones','*.do*[xc]',sql)
 
-    print "---------------archivos de DIGESTO que faltan procesar  *.DOC* -----------------:"
+    print "\n\n---------------archivos de RESOLUCIONES FI que faltan procesar  *.DOC* -----------------:"
 
-    procesar_words(diff)
 
     print "en FS:"+str(countinFS)
     print "en DB:"+str(countinDB)
+
+
+    procesar_words(diff,'1')
+
+
+
+    ###################### RESOLUCIONES FI  (.PDF*)  #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='1' AND nombre LIKE '%.pdf' "
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/facultad/resoluciones/','*.pdf',sql)
+
+    print "\n\n----------------archivos de RESOLUCIONES FI que faltan procesar  *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'1')
+
+
+
+    ###################### ORDENANZAS FI (.DOC*)  #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='2' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/facultad/ordenanzas','*.do*[xc]',sql)
+
+    print "\n\n---------------archivos de ORDENANZAS FI que faltan procesar  *.DOC* -----------------:"
+
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+
+    procesar_words(diff,'2')
+
+
+
+    ###################### ORDENANZAS FI  (.PDF*)  #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='2' AND nombre LIKE '%.pdf' "
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/facultad/ordenanzas/','*.pdf',sql)
+
+    print "\n\n----------------archivos de ORDENANZAS FI que faltan procesar  *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'2')
+
+
+
+    ###################### DIGESTO (.DOC*)  #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='3' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/intranet/repo/digesto','*.do*[xc]',sql)
+
+    print "\n\n---------------archivos de DIGESTO que faltan procesar  *.DOC* -----------------:"
+
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+
+    procesar_words(diff,'3')
+
+
 
     ###################### DIGESTO (.PDF*)  #######################
 
-    sql = " SELECT archivo FROM ocr WHERE tipo='3'AND nombre LIKE '%.pdf' "
-
+    sql = " SELECT archivo FROM ocr WHERE tipo='3' AND nombre LIKE '%.pdf' "
     diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/intranet/repo/digesto','*.pdf',sql)
 
-    print "---------------archivos de DIGESTO que faltan procesar  *.PDF -----------------:"
-
-    procesar_pdf(diff)
+    print "\n\n----------------archivos de DIGESTO que faltan procesar  *.PDF -----------------:"
 
     print "en FS:"+str(countinFS)
     print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'3')
+
+
 
     ###################### DOC CONSEJO (.DOC*) #######################
 
-    sql = " SELECT archivo FROM ocr WHERE tipo='4'AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+    sql = " SELECT archivo FROM ocr WHERE tipo='4' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
 
-    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/intranet/repo/documentos','*.doc*',sql)
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/intranet/repo/documentos','*.do*[xc]',sql)
 
-    print "---------------archivos de DOC CONSEJO que faltan procesar *.DOC* -----------------:"
-
-    procesar_words(diff)
+    print "\n\n----------------archivos de DOC CONSEJO que faltan procesar *.DOC* -----------------:"
 
     print "en FS:"+str(countinFS)
     print "en DB:"+str(countinDB)
+
+    procesar_words(diff,'4')
+
 
     ###################### DOC CONSEJO (.PDF) #######################
 
-    sql = " SELECT archivo,id FROM ocr WHERE tipo='4'AND nombre LIKE '%.pdf' "
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='4' AND nombre LIKE '%.pdf' "
 
     diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/intranet/repo/documentos','*.pdf',sql)
 
-    print "---------------archivos de DOC CONSEJO que faltan procesar *.PDF -----------------:"
-
-    procesar_pdf(diff)
+    print "\n\n----------------archivos de DOC CONSEJO que faltan procesar *.PDF -----------------:"
 
     print "en FS:"+str(countinFS)
     print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'4')
+
+    ###################### RESOL UNLP (.PDF) #######################
+
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='5' AND nombre LIKE '%.pdf' "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/unlp/resoluciones','*.pdf',sql)
+
+    print "\n\n----------------archivos de RESOL UNLP que faltan procesar *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'5')
+
+
+    ###################### ORD UNLP (.PDF) #######################
+
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='6' AND nombre LIKE '%.pdf' "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/unlp/ordenanzas','*.pdf',sql)
+
+    print "\n\n----------------archivos de ORD UNLP que faltan procesar *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'6')
+
+
+    ###################### LEGISLACION NACION (.PDF) #######################
+
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='7' AND nombre LIKE '%.pdf' "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/institucional/legislacion/nacional','*.pdf',sql)
+
+    print "\n\n----------------archivos de LEGISLACION -> NACIONAL que faltan procesar *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'7')
+
+
+    ###################### CONCURSOS (.DOC*) #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='8' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/administracion/concursos/archivos','*.do*[xc]',sql)
+
+    print "\n\n----------------archivos de CONCURSOS que faltan procesar *.DOC* -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_words(diff,'8')
+
+
+    ###################### CONCURSOS (.PDF) #######################
+
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='8' AND nombre LIKE '%.pdf' "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/administracion/concursos/archivos','*.pdf',sql)
+
+    print "\n\n----------------archivos de CONCURSOS que faltan procesar *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'8')
+
+
+    ###################### PERSONAL (.DOC*) #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='9' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/administracion/personal/archivos','*.do*[xc]',sql)
+
+    print "\n\n----------------archivos de PERSONAL que faltan procesar *.DOC* -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_words(diff,'9')
+
+
+    ###################### PERSONAL (.PDF) #######################
+
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='9' AND nombre LIKE '%.pdf' "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/administracion/personal/archivos','*.pdf',sql)
+
+    print "\n\n----------------archivos de PERSONAL que faltan procesar *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'9')
+
+
+    ###################### DOCENTES (.DOC*) #######################
+
+    sql = " SELECT archivo FROM ocr WHERE tipo='10' AND (nombre LIKE '%.doc' OR nombre LIKE '%.docx') "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/docentes/archivos','*.do*[xc]',sql)
+
+    print "\n\n----------------archivos de DOCENTES que faltan procesar *.DOC* -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_words(diff,'10')
+
+
+    ###################### DOCENTES (.PDF) #######################
+
+    sql = " SELECT archivo,id FROM ocr WHERE tipo='10' AND nombre LIKE '%.pdf' "
+
+    diff, countinDB, countinFS = lista_diferencia_DBFS('/var/www/sitio/docentes/archivos','*.pdf',sql)
+
+    print "\n\n----------------archivos de DOCENTES que faltan procesar *.PDF -----------------:"
+
+    print "en FS:"+str(countinFS)
+    print "en DB:"+str(countinDB)
+
+    procesar_pdf(diff, procesarPDF,'10')
+
+
 
     # Actualizar y rellenar otros campos:
 
     sql = " SELECT archivo,id FROM ocr WHERE fecha IS NULL "
-    insertar("UPDATE ocr SET nombre=SUBSTRING_INDEX(archivo,'/',-1)", "", "")
+    insertar("UPDATE ocr SET nombre=SUBSTRING_INDEX(archivo,'/',-1)", "", "","")
 
     for t in getData(sql):
         d = modification_date(t[0])
-        print "id: "+str(t[1])+" ->"+d
-        insertar("UPDATE ocr SET fecha='"+d+"' WHERE id='"+str(t[1])+"'", "", "")
-
+        #print "id: "+str(t[1])+" ->"+d
+        insertar("UPDATE ocr SET fecha='"+d+"' WHERE id='"+str(t[1])+"'", "", "","")
 except:
     print "Error inesperado, eliminando posibles temporales..."
     os.system("rm -f file.tiff && rm -f ocr.txt && rm -f tmp.pdf") # && find /tmp -maxdepth 1 -type f -name 'magick-*' -delete")    
-    raise
+    #raise
